591f35c71a58f3ba5a11348776102a2d83690db7
Thomas Lerchbaumer <thomas.lerchbaumer@stud.fh-campuswien.ac.at>
2020-12-17 17:11:02 +0100

    first commit

---
+++ src/main/java/com/dithub/weather/Main.java
@@ -0,0 +1,30 @@
+package com.dithub.weather;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("ui.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        primaryStage.setTitle("Weather forecast powered by Dithub");
+        primaryStage.setScene(new Scene(root, 800, 400));
+        primaryStage.show();
+
+    }
+}
