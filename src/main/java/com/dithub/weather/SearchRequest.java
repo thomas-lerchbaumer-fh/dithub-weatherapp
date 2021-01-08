ee334cdb93266082fdd11474a9076e6787dd3bf4
Dominic Duda <Dominic.Duda@gmx.at>
2021-01-08 15:07:21 +0100

    Deleted class SearchRequest

--- src/main/java/com/dithub/weather/SearchRequest.java
+++
@@ -1,9 +0,0 @@
-package com.dithub.weather;
-
-public class SearchRequest {
-
-    //pulling the data from api creating object for each day sotring them in object / arraylist
-    // on app launch ip lookup should be done and the user's location should be found please check for api call that enables this behavior
-
-
-}
