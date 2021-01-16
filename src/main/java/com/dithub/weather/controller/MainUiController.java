8551c1f2cf353412c096acf7e3739525764cd4a1
thomas-lerchbaumer-fh <thomas.lerchbaumer@stud.fh-campuswien.ac.at>
2021-01-16 18:26:25 +0100

    files changed: +- MainUiController
    What was changed?
    -code cleanup
    -added popup when search was not successfull
    -changed time to utc and adding offset to it
    -converting "Umlaute" now if user searches for e.g. Sankt Pölten
    -converting "Umlaute" when displayed in UI (using unicode)
    -changed local date to en (so full UI is in english now)

--- src/main/java/com/dithub/weather/controller/MainUiController.java
+++ src/main/java/com/dithub/weather/controller/MainUiController.java
@@ -6,20 +6,24 @@ import com.dithub.weather.WeatherForecast;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
 import javafx.scene.image.Image;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -90,74 +94,111 @@ public class MainUiController {
         APIHandler data = new APIHandler();
 
         //Get Search Request
-
         searchBox.setOnAction(new EventHandler<ActionEvent>() {
             String tfData = "";
             @Override
             public void handle(ActionEvent event) {
-                tfData = searchBox.getText();
-                ArrayList<Object> oneDayWeatherDataRequest = data.getCurrentApiData(tfData);
-                WeatherDay oneDayWeatherRequest = new WeatherDay(oneDayWeatherDataRequest);
-                ArrayList<Object> dailyForecastDataRequest = data.getForecastApiData(tfData);
-                WeatherForecast dailyForecastRequest = new WeatherForecast(dailyForecastDataRequest,5);
-
-                ArrayList dayList = getDaysOfWeek();
-
-                String city = oneDayWeatherRequest.cityOneDay;
-                String country = oneDayWeatherRequest.countryOneDay;
-                String tempCel = oneDayWeatherRequest.tempOneDay;
-                String iconIdent = oneDayWeatherRequest.iconOneDay;
-                int timezoneOneDay = oneDayWeatherRequest.timezoneOffsetOneDay;
-                int timezoneForecast = dailyForecastRequest.timezoneOffsetForecast;
-                String imgPath = "/com/dithub/weather/img/icons/";
-                Image img = new Image(getClass().getResource(imgPath).toString());
-
-                //setting infos from API to UI lables/img
-                lblCity.setText(city+ ", " + country);
-                lblCurrTime.setText(getTime());
-                lblTemp.setText(tempCel);
-                lblCurrDay.setText(getDay());
-                imgWeatherImg.setImage(new Image(imgPath+iconIdent+".png"));
-
-                //day 2
-                lblday2.setText(String.valueOf(dayList.get(0)));
-                imgWeatherImg2.setImage(new Image(imgPath+dailyForecastRequest.iconForecast[0]+".png"));
-                lblday2temp.setText(dailyForecastRequest.tempForecast[0]);
-
-                //day 3
-                lblday3.setText(String.valueOf(dayList.get(1)));
-                imgWeatherImg3.setImage(new Image(imgPath+dailyForecastRequest.iconForecast[1]+".png"));
-                lblday3temp.setText(dailyForecastRequest.tempForecast[1]);
-
-                //day 4
-                lblday4.setText(String.valueOf(dayList.get(2)));
-                imgWeatherImg4.setImage(new Image(imgPath+dailyForecastRequest.iconForecast[2]+".png"));
-                lblday4temp.setText(dailyForecastRequest.tempForecast[2]);
-
-                //day 5
-                lblday5.setText(String.valueOf(dayList.get(3)));
-                imgWeatherImg5.setImage(new Image(imgPath+dailyForecastRequest.iconForecast[3]+".png"));
-                lblday5temp.setText(dailyForecastRequest.tempForecast[3]);
-
-                //day 6
-                lblday6.setText(String.valueOf(dayList.get(4)));
-                imgWeatherImg6.setImage(new Image(imgPath+dailyForecastRequest.iconForecast[4]+".png"));
-                lblday6temp.setText(dailyForecastRequest.tempForecast[4]);
-
-                getDaysOfWeek();
+                tfData =  replaceSpecialSignInput(searchBox.getText());
+                try{
+                    setSceneProperties(tfData);
+                   }
+                catch (Exception ex0){
+                    searchRequestNotFound(tfData);
+                }
             }
         });
 
+     setSceneProperties("");
+
+    }
+    //formatting current time (on app launch)
+    private String getTime(int offset){
+        ZonedDateTime utc = ZonedDateTime.now(ZoneOffset.UTC);
+        utc = utc.plusSeconds(offset);
+
+        return utc.format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
+    //formatting current day (on app launch)
+    private String getDay(){
+        Date currentDay = new Date();
+        DateFormat format = new SimpleDateFormat("EEE d, MMM, YYYY",new Locale("en"));
+        return format.format(currentDay);
+    }
+
+    //retrieving 5 upcoming days
+    private ArrayList getDaysOfWeek(){
+        ArrayList dayList = new ArrayList();
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter dayFormat = DateTimeFormatter.ofPattern("EEE d",new Locale("en"));
+        for (int i = 1; i <= 5; i++){
+            dayList.add(dayFormat.format(today.plusDays(i)));
+        }
+        return dayList;
+    }
+
+    private void searchRequestNotFound(String city){
+        //creating alert window when api did not return any search request
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Not Found");
+        alert.setHeaderText("Sorry, we were not able to find: " + city + ", please try again.");
+        alert.setContentText(replaceSpecialSignOutput("Please be aware that shortcodes etc. cannot be found e.g. 'St. Pölten' won't work but 'Sankt Pölten' would work"));
+        //adding stylesheet to alert window
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.getStylesheets().add(
+                getClass().getResource("/com/dithub/weather/css/style.css/").toExternalForm());
+        dialogPane.getStyleClass().add("notFound");
+        alert.showAndWait();
+    }
+
+    private String replaceSpecialSignOutput(String input){
+        //replaces all "Umlaute" with unicode
+        String output = input.replace("ü", "\u00fc")
+                .replace("ö", "\u00f6")
+                .replace("ä", "\u00e4")
+                .replace("ß", "\u00df");
+        return output;
+
+    }
+
+    private String replaceSpecialSignInput(String input){
+        // in case of user searches for e.g. Sankt Pölten  --> ö will be replaced with oe so search request is sucessfull
+        String output = input.replace("\u00fc", "ue")
+                .replace("\u00f6", "oe")
+                .replace("\u00e4", "ae")
+                .replace("\u00df", "ss")
+                .replaceAll("\u00dc(?=[a-z\u00e4\u00f6\u00fc\u00df ])", "Ue")
+                .replaceAll("\u00d6(?=[a-z\u00e4\u00f6\u00fc\u00df ])", "Oe")
+                .replaceAll("\u00c4(?=[a-z\u00e4\u00f6\u00fc\u00df ])", "Ae")
+                .replace("\u00dc", "UE")
+                .replace("\u00d6", "OE")
+                .replace("\u00c4", "AE");
+        return output;
+
+    }
+
+
+    private void setSceneProperties(String searchRequest){
+        APIHandler data = new APIHandler();
 
         //pulling data from weather data class
 
-        String location = data.getCurrentLocation();
-        ArrayList<Object> oneDayWeatherData = data.getCurrentApiData(location);
-        WeatherDay oneDayWeather = new WeatherDay(oneDayWeatherData);
+        ArrayList<Object> oneDayWeatherData;
+        WeatherDay oneDayWeather;
+        ArrayList<Object> dailyForecastData;
+        WeatherForecast dailyForecast;
 
-        ArrayList<Object> dailyForecastData = data.getForecastApiData(data.getCurrentLocation());
-
-        WeatherForecast dailyForecast = new WeatherForecast(dailyForecastData,5);
+        if(searchRequest == ""){
+            String location = data.getCurrentLocation();
+            oneDayWeatherData = data.getCurrentApiData(location);
+            oneDayWeather = new WeatherDay(oneDayWeatherData);
+            dailyForecastData = data.getForecastApiData(data.getCurrentLocation());
+            dailyForecast = new WeatherForecast(dailyForecastData,5);
+        }else {
+            oneDayWeatherData = data.getCurrentApiData(searchRequest);
+            oneDayWeather = new WeatherDay(oneDayWeatherData);
+            dailyForecastData = data.getForecastApiData(searchRequest);
+            dailyForecast = new WeatherForecast(dailyForecastData,5);
+        }
 
         ArrayList dayList = getDaysOfWeek();
 
@@ -167,19 +208,14 @@ public class MainUiController {
         String iconIdent = oneDayWeather.iconOneDay;
         int timezoneOneDay = oneDayWeather.timezoneOffsetOneDay;
         int timezoneForecast = dailyForecast.timezoneOffsetForecast;
-        System.out.println(timezoneForecast);
-        System.out.println(timezoneOneDay);
 
-        long epoch = System.currentTimeMillis()/1000;
-        long correctTimezone = (epoch+timezoneOneDay);
-        System.out.println(epoch);
 
         String imgPath = "/com/dithub/weather/img/icons/";
         Image img = new Image(getClass().getResource(imgPath).toString());
 
         //setting infos from API to UI lables/img
-        lblCity.setText(city+ ", " + country);
-        lblCurrTime.setText(getTime());
+        lblCity.setText(replaceSpecialSignOutput(city)+ ", " + replaceSpecialSignOutput(country));
+        lblCurrTime.setText(getTime(timezoneOneDay));
         lblTemp.setText(tempCel);
         lblCurrDay.setText(getDay());
         imgWeatherImg.setImage(new Image(imgPath+iconIdent+".png"));
@@ -211,29 +247,6 @@ public class MainUiController {
 
         getDaysOfWeek();
 
-    }
-    //formatting current time (on app launch)
-    private String getTime(){
-      Date currentTime = new Date();
-      DateFormat format = new SimpleDateFormat("HH:mm");
-      return format.format(currentTime);
-    }
-    //formatting current day (on app launch)
-    private String getDay(){
-        Date currentDay = new Date();
-        DateFormat format = new SimpleDateFormat("EEE d, MMM, YYYY");
-        return format.format(currentDay);
-    }
-
-    //retrieving 5 upcoming days
-    private ArrayList getDaysOfWeek(){
-        ArrayList dayList = new ArrayList();
-        LocalDate today = LocalDate.now();
-        DateTimeFormatter dayFormat = DateTimeFormatter.ofPattern("EEE d");
-        for (int i = 1; i <= 5; i++){
-            dayList.add(dayFormat.format(today.plusDays(i)));
-        }
-        return dayList;
     }
 
 }
